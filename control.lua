5a3bca76760a4660d035aaa96317a94ef8c02b5c
Mark Sanders <mark.something@gmail.com>
2018-02-21 07:32:32 +0100

    Various optimizations (Thanks Nexela).

--- control.lua
+++ control.lua
@@ -1,54 +1,35 @@
 local floor = math.floor
+local rand = math.random
 
-local function is_clean_water_str(str)
-	for _,r in pairs{"water", "deepwater"} do
-		if str == r then return true end
-	end
-	return false
-end
-
-local function is_green_water_str(str)
-	for _,r in pairs{"water-green", "deepwater-green"} do
-		if str == r then return true end
-	end
-	return false
-end
-
-local function is_water_str(str)
-	for _,r in pairs{"water", "deepwater", "water-green", "deepwater-green"} do
-		if str == r then return true end
-	end
-	return false
-end
-
-local function initialize()
-	global.corpses = {}
-	global.rng     = game.create_random_generator()
-end
+local waters = {
+	clean = {["water"] = true , ["deepwater"] = true},
+	green = {["water-green"] = true, ["deepwater-green"] = true}
+}
 
 script.on_configuration_changed(function()
-	initialize()
+	global.corpses = {}
 end)
 
 script.on_init(function()
-	initialize()
+	global.corpses = global.corpses or {}
 end)
 
 local function do_the_ripple(player)
 	local r = 2.5
 	local p = player.position
 	local surface = player.surface
-	if is_clean_water_str(surface.get_tile(p).name) then
-		if surface.count_tiles_filtered{area = {{p.x - r, p.y - r}, {p.x + r, p.y + r}}, name = "water", limit = 25} +
-			surface.count_tiles_filtered{area = {{p.x - r, p.y - r}, {p.x + r, p.y + r}}, name = "deepwater", limit = 25} >= 25
+	local area = {{p.x - r, p.y - r}, {p.x + r, p.y + r}} -- Do the math once
+	if waters.clean[surface.get_tile(p).name] then
+		if surface.count_tiles_filtered{area = area, name = "water", limit = 25} +
+			surface.count_tiles_filtered{area = area, name = "deepwater", limit = 25} >= 25
 		then
-			surface.create_entity{name = "water-ripple" .. global.rng(1, 4) .. "-smoke", position = p}
+			surface.create_entity{name = "water-ripple" .. rand(1, 4) .. "-smoke", position = p}
 		end
-	elseif is_green_water_str(surface.get_tile(p).name) then
-		if surface.count_tiles_filtered{area = {{p.x - r, p.y - r}, {p.x + r, p.y + r}}, name = "water-green", limit = 25} +
-			surface.count_tiles_filtered{area = {{p.x - r, p.y - r}, {p.x + r, p.y + r}}, name = "deepwater-green", limit = 25} >= 25
+	elseif waters.green[surface.get_tile(p).name] then
+		if surface.count_tiles_filtered{area = area, name = "water-green", limit = 25} +
+			surface.count_tiles_filtered{area = area, name = "deepwater-green", limit = 25} >= 25
 		then
-			surface.create_entity{name = "greenwater-ripple" .. global.rng(1, 4) .. "-smoke", position = p}
+			surface.create_entity{name = "greenwater-ripple" .. rand(1, 4) .. "-smoke", position = p}
 		end
 	end
 end
@@ -59,20 +40,18 @@ script.on_event(defines.events.on_tick, function(event)
 		-- Clean up corpses in water
 		local r = 1
 		for _,v in pairs(global.corpses) do
-			local corpses = game.surfaces[v[1]].find_entities_filtered{area = {{v[2] - r, v[3] - r}, {v[2] + r, v[3] + r}}, type = "corpse"}
-			if corpses then
-				for i=#corpses,1,-1 do
-					--todo: maybe I can change the fade times to be short instead of just destroying them.
-					corpses[i].destroy()
-				end
+			for _, corpse in pairs(game.surfaces[v[1]].find_entities_filtered{area = {{v[2] - r, v[3] - r}, {v[2] + r, v[3] + r}}, type = "corpse"}) do
+				--todo: maybe I can change the fade times to be short instead of just destroying them.
+				--Note to Nexela add corpse::time_to_live read/write
+				corpse.destroy()
 			end
 		end
 		global.corpses = {}
 	end
 	if event.tick % 60 == 0 then
 		-- Ripple for players in water
-		for _,player in pairs(game.players) do
-			if player.connected and player.character then
+		for _,player in pairs(game.connected_players) do
+			if player.character then
 				do_the_ripple(player)
 			end
 		end
@@ -81,12 +60,12 @@ end)
 
 script.on_event(defines.events.on_player_changed_position, function(e)
 	local player = game.players[e.player_index]
-	if player.connected and player.character then
+	if player.character then
 		do_the_ripple(player)
 		--@todo: Vehicles should splash at the wheels
-		if is_clean_water_str(player.surface.get_tile(player.position).name) then
+		if waters.clean[player.surface.get_tile(player.position).name] then
 			player.surface.create_entity{name = "water-splash-smoke", position = player.position}
-		elseif is_green_water_str(player.surface.get_tile(player.position).name) then
+		elseif waters.green[player.surface.get_tile(player.position).name] then
 			player.surface.create_entity{name = "greenwater-splash-smoke", position = player.position}
 		end
 	end
@@ -97,7 +76,8 @@ script.on_event(defines.events.on_entity_died, function(event)
 	if event.entity and event.entity.position and event.entity.surface then
 		local surface = event.entity.surface
 		local p = event.entity.position
-		if is_water_str(surface.get_tile(p).name) then
+		local tile_name = surface.get_tile(p).name
+		if waters.clean[tile_name] or waters.green[tile_name] then
 			global.corpses[surface.index .. "," .. floor(p.x) .. "," .. floor(p.y)] = {surface.index, floor(p.x), floor(p.y)}
 		end
 	end
