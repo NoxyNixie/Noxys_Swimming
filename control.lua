c1f1c1ef60cbf51018bfde864634541b9ea5fff8
Mark Sanders <mark.something@gmail.com>
2019-07-06 17:18:16 +0200

    Make use of the corpses data in the on_post_entity_died event (optimization). Dying entities now leave a ripple.

--- control.lua
+++ control.lua
@@ -1,4 +1,3 @@
-local floor = math.floor
 local rand = math.random
 
 local waters = {
@@ -14,10 +13,8 @@ script.on_init(function()
 	global.corpses = global.corpses or {}
 end)
 
-local function do_the_ripple(player)
+local function ripple_at_position(p, surface)
 	local r = 2.5
-	local p = player.position
-	local surface = player.surface
 	local area = {{p.x - r, p.y - r}, {p.x + r, p.y + r}} -- Do the math once
 	if waters.clean[surface.get_tile(p).name] then
 		if surface.count_tiles_filtered{area = area, name = "water", limit = 25} +
@@ -34,20 +31,11 @@ local function do_the_ripple(player)
 	end
 end
 
+local function do_the_ripple(player)
+	ripple_at_position(player.position, player.surface)
+end
+
 script.on_event(defines.events.on_tick, function(event)
-	event.tick = event.tick + 1
-	if event.tick % 30 == 0 then
-		-- Clean up corpses in water
-		local r = 1
-		for _,v in pairs(global.corpses) do
-			for _, corpse in pairs(game.surfaces[v[1]].find_entities_filtered{area = {{v[2] - r, v[3] - r}, {v[2] + r, v[3] + r}}, type = "corpse"}) do
-				--todo: maybe I can change the fade times to be short instead of just destroying them.
-				--Note to Nexela add corpse::time_to_live read/write
-				corpse.destroy()
-			end
-		end
-		global.corpses = {}
-	end
 	if event.tick % 60 == 0 then
 		-- Ripple for players in water
 		for _,player in pairs(game.connected_players) do
@@ -71,14 +59,9 @@ script.on_event(defines.events.on_player_changed_position, function(e)
 	end
 end)
 
--- Mark entities that die in water for cleaning up their corpses later
-script.on_event(defines.events.on_entity_died, function(event)
-	if event.entity and event.entity.position and event.entity.surface then
-		local surface = event.entity.surface
-		local p = event.entity.position
-		local tile_name = surface.get_tile(p).name
-		if waters.clean[tile_name] or waters.green[tile_name] then
-			global.corpses[surface.index .. "," .. floor(p.x) .. "," .. floor(p.y)] = {surface.index, floor(p.x), floor(p.y)}
-		end
+script.on_event(defines.events.on_post_entity_died, function(event)
+	for _,v in pairs(event.corpses) do
+		ripple_at_position(v.position, v.surface)
+		v.destroy()
 	end
 end)
